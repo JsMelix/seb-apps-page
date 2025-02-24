9496f0f84674e3aff0cc9401cc9233dcbdc0cf0b
JsMelix <juansmelo03@gmail.com>
2025-02-23 21:33:50 -0500

    addpage

--- script.js
+++ script.js
@@ -7,9 +7,24 @@ document.addEventListener("DOMContentLoaded", function () {
         if (index < text.length) {
             textElement.innerHTML += text.charAt(index);
             index++;
-            setTimeout(typeEffect, 100); // Velocidad del tipeo
+            setTimeout(typeEffect, 100);
         }
     }
 
     typeEffect();
+
+    // Efecto de tipeo en la biografía
+    const bioTextElement = document.getElementById("bio-text");
+    const bioText = "I'm Kareem Elramady, a passionate web developer focused on creating pixel-perfect and dynamic websites. With years of experience, I bring ideas to life through clean and interactive design.";
+    let bioIndex = 0;
+
+    function typeBioEffect() {
+        if (bioIndex < bioText.length) {
+            bioTextElement.innerHTML += bioText.charAt(bioIndex);
+            bioIndex++;
+            setTimeout(typeBioEffect, 50);
+        }
+    }
+
+    setTimeout(typeBioEffect, 2000); // Espera antes de iniciar
 });
